7fbb541d446877335aa60bfd6a0b55582bf2704c
Adrian Verban <verbanady@gmail.com>
2019-02-06 20:53:07 +0200

    Added util.sh to commit, forgot about it.

--- util.sh
+++ util.sh
@@ -16,35 +16,64 @@ config_property() {
   fi
 }
 
+trim() {
+    [[ "$1" =~ [^[:space:]](.*[^[:space:]])? ]]
+    printf "%s" "$BASH_REMATCH"
+}
+
 deploy_plugins() {
 cat <<EOF |
-`ls -l "$LIBRARIES" | sed -En 's/(\-.*)(libQt[0-9][a-zA-Z]*)(\.so.[0-9])/\2/ p'`
+`find "$LIBRARIES" | sed -En 's/(\/.*)(libQt[0-9][a-zA-Z]*)(\.so.[0-9])/\2/ p'`
 EOF
 while IFS= read lib; do
   if [ "$lib" == "libQt5Gui" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" platforms/libqxcb.so platforminputcontexts xcbglintegrations imageformats
-    if [ -f "$PLUGINS/imageformats/libqsvg.so" ]; then
-      rm "$PLUGINS/imageformats/libqsvg.so"
+    if [ $? -eq 1 ]; then
+      return 1
     fi
   elif [ "$lib" == "libQt5OpenGL" -o "$lib" == "libQt5XcbQpa" -o "$lib" == "libxcb-glx" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" xcbglintegrations
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   elif [ "$lib" == "libQt5Svg" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" iconengines/libqsvgicon.so imageformats/libqsvg.so
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   elif [ "$lib" == "libQt5PrintSupport" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" printsupport/libcupsprintersupport.so
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   elif [ "$lib" == "libQt5Network" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" bearer
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   elif [ "$lib" == "libQt5Sql" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" sqldrivers
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   elif [ "$lib" == "libQt5Positioning" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" position
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   elif [ "$lib" == "libQt5Multimedia" ]; then
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" mediaservice audio
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   elif [ "$lib" == "libQt5WebEngineCore" ]; then
     mkdir -p "$PLUGINS../libexec"
     mkdir -p "$PLUGINS../resources"
     mkdir -p "$PLUGINS../translations"
     ./deploy-plugins.sh "$PLUGINS" "$QT_SOURCE_PLUGINS" ../libexec ../resources ../translations
+    if [ $? -eq 1 ]; then
+      return 1
+    fi
   fi
 done
 }
